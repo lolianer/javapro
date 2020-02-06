5fba265d90a3b6cc5c818945e83f9d823b8bfb85
asus <1172595124@qq.com>
2020-02-06 15:48:05 +0800

    123

---
+++ src/com/neuedu/test/MyTest.java
@@ -0,0 +1,12 @@
+package com.neuedu.test;
+
+public class MyTest {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        System.out.println(a+b);
+
+    }
+
+
+}
